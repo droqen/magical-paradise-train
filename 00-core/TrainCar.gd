14d5367d799b4e69c49cf5bd51a3afd3f137962a
Joeri Bakker <bakkerjoeri@gmail.com>
2021-07-07 21:55:15 +0200

    Rewrite animation triggers

--- 00-core/TrainCar.gd
+++ 00-core/TrainCar.gd
@@ -6,40 +6,22 @@ var state = "riding";
 var current_animation;
 
 func _process(_delta):
-	if state == "riding" && !$CarRiding.is_playing():
-		if $DoorsOpening.is_playing():
-			$DoorsOpening.stop();
-
-		$CarRiding.play();
-	
 	if state == "doors_opening":
-		if $CarRiding.is_playing():
-			$CarRiding.stop();
-		
-		if !$DoorsOpening.is_playing():
-			$DoorsOpening.play("DoorsOpening");
-			$DoorsOpening.connect("animation_finished", self, "_on_finished_doors_opening", [], CONNECT_ONESHOT);
-	
-	if state == "doors_open":
-		$DoorsOpening.stop();
+		$DoorAnimations.play("DoorsOpening");
+		yield($DoorAnimations, "animation_finished");
+		emit_signal("doors_opened");
+		state = "doors_open";
 	
 	if state == "doors_closing":
-		if !$DoorsOpening.is_playing():
-			$DoorsOpening.play_backwards("DoorsOpening");
-			$DoorsOpening.connect("animation_finished", self, "_on_finished_doors_closing", [], CONNECT_ONESHOT);
-
-func start_riding():
-	state = "doors_riding"
-
-func close_doors():
-	state = "doors_closing"
+		$DoorAnimations.play("DoorsOpening", -1, -1.0, true);
+		yield($DoorAnimations, "animation_finished");
+		state = "riding";
+	
+	if state == "riding":
+		$DoorAnimations.play("CarRiding");
 
 func open_doors():
-	state = "doors_opening"
+	state = "doors_opening";
 
-func _on_finished_doors_closing():
-	self.start_riding();
-
-func _on_finished_doors_opening():
-	state = "doors_open";
-	emit_signal("doors_opened");
+func close_doors():
+	state = "doors_closing";

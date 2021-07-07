60af6bcd4bbcb65b74b2fd03d5cbd42ecc5b8ef2
Joeri Bakker <bakkerjoeri@gmail.com>
2021-07-07 22:49:50 +0200

    Add a little waiting time between games

--- 00-core/TrainJamMaster.gd
+++ 00-core/TrainJamMaster.gd
@@ -11,12 +11,30 @@ var preloaded_microgame: MicrogameMetadata = null
 export(Curve)var viewport_slide_in
 export(Curve)var viewport_slide_out
 var slide_progress = 0
+var chill_progress = 0;
 var sliding_in = false
 var sliding_out = false
+var chilling_a_bit = false;
 
 func _ready():
+	go_to_first_scene()
+	slide_progress = 0
+	
+func go_to_first_scene():
 	load_next_microgame()
-	slide_progress = 1
+	sliding_in = false
+	sliding_out = false
+	chilling_a_bit = true;
+	chill_progress = 0;
+	slide_progress = 0;
+
+func go_to_next_scene():
+	load_next_microgame()
+	sliding_in = false
+	sliding_out = true
+	chilling_a_bit = false;
+	chill_progress = 0;
+	slide_progress = 0;
 
 func load_next_microgame():
 	if test_microgame_scene:
@@ -35,9 +53,10 @@ func load_next_microgame():
 
 func load_microgame(mg: MicrogameMetadata):
 	preloaded_microgame = mg
-	$"../TrainCar".close_doors();
 	sliding_in = false
 	sliding_out = true
+	chilling_a_bit = false;
+	chill_progress = 0;
 	slide_progress = 0
 
 func unload_current_scene():
@@ -60,8 +79,8 @@ func set_current_microgame(mg: MicrogameMetadata):
 	scene.owner = vp.owner
 	vpc.show()
 	
-	scene.connect("player_won", self, "load_next_microgame")
-	scene.connect("player_lost", self, "load_next_microgame")
+	scene.connect("player_won", self, "go_to_next_scene")
+	scene.connect("player_lost", self, "go_to_next_scene")
 	
 	nvw.match_board_size()
 	
@@ -70,20 +89,29 @@ func _process(delta):
 	if preloaded_microgame:
 		if sliding_out:
 			slide_progress += delta * 0.85
-			if slide_progress >= 1:
-				set_current_microgame(preloaded_microgame)
-				$"../TrainCar".open_doors();
-				sliding_out = false
-				sliding_in = true
-				slide_progress = 0
 			_update_slide_position()
+			if slide_progress >= 1:
+				$"../TrainCar".close_doors();
+				sliding_out = false
+				sliding_in = false
+				chilling_a_bit = true
 		elif sliding_in:
 			slide_progress += delta * 0.5
 			_update_slide_position()
 			if slide_progress >= 1:
 				preloaded_microgame = null
 				sliding_in = false
-#				_update_slide_position()
+				sliding_out = false;
+				chilling_a_bit = false;
+		elif chilling_a_bit:
+			chill_progress += delta
+			if chill_progress >= 2.5:
+				set_current_microgame(preloaded_microgame)
+				$"../TrainCar".open_doors();
+				chill_progress = 0;
+				chilling_a_bit = false
+				sliding_out = false
+				sliding_in = true
 
 func _update_slide_position():
 	var nvw = $"../MarginContainer/NavdiViewerWindow"
@@ -92,7 +120,7 @@ func _update_slide_position():
 	if sliding_in:
 		offset = Vector2.RIGHT * window_size * viewport_slide_in.interpolate(slide_progress)
 	elif sliding_out:
-		offset = Vector2.LEFT * window_size * viewport_slide_out.interpolate(slide_progress)
+		offset = Vector2.LEFT * window_size * viewport_slide_out.interpolate(slide_progress) * 1.1
 	nvw.refresh()
 	for child in nvw.get_children():
 		child.rect_position += offset

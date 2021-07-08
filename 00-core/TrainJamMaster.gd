1cc3bda4e9f849b627035e7f232ee0f3f6c7c285
droqen <hi@droqen.com>
2021-07-08 08:51:48 -0400

    game enter animation cleaned up, delays in set_microgame_state
    * note: i made the train car transparent to show the effect, i think having the windows actually open would be cool

--- 00-core/TrainJamMaster.gd
+++ 00-core/TrainJamMaster.gd
@@ -10,33 +10,30 @@ var current_microgame: MicrogameMetadata = null
 var preloaded_microgame: MicrogameMetadata = null
 export(Curve)var viewport_slide_in
 export(Curve)var viewport_slide_out
-var slide_progress = 0
-var chill_progress = 0;
-var sliding_in = false
-var sliding_out = false
-var chilling_a_bit = false;
+
+onready var train_car = $"../TrainCar"
+
+enum MgState{
+	GameSlidingIn = 15,
+	DoorsOpening = 16,
+	PlayingTheGame = 25,
+	DoorsClosing = 34,
+	GameSlidingOut = 35,
+	ChillingABit = 45,
+}
+
+var microgameState = MgState.ChillingABit
+var stateTimer = 0.0
+var stateDuration = 1.0
 
 func _ready():
-	go_to_first_scene()
-	slide_progress = 0
-	
-func go_to_first_scene():
-	load_next_microgame()
-	sliding_in = false
-	sliding_out = false
-	chilling_a_bit = true;
-	chill_progress = 0;
-	slide_progress = 0;
+	load_first_game()
 
-func go_to_next_scene():
-	load_next_microgame()
-	sliding_in = false
-	sliding_out = true
-	chilling_a_bit = false;
-	chill_progress = 0;
-	slide_progress = 0;
+func load_first_game():
+	set_microgame_state(MgState.ChillingABit)
+	stateTimer = stateDuration # instantly slide in
 
-func load_next_microgame():
+func preload_next_microgame():
 	if test_microgame_scene:
 		var test_microgame = MicrogameMetadata.new()
 		test_microgame.microgame_name = "Test"
@@ -44,20 +41,15 @@ func load_next_microgame():
 		test_microgame.microgame_scene = test_microgame_scene
 		if not test_microgame_repeating:
 			test_microgame_scene = null
-		load_microgame(test_microgame)
+		preload_microgame(test_microgame)
 	else:
-		load_microgame(microgames[microgame_index])
+		preload_microgame(microgames[microgame_index])
 		microgame_index += 1
 		if microgame_index >= len(microgames):
 			microgame_index = 0
 
-func load_microgame(mg: MicrogameMetadata):
+func preload_microgame(mg: MicrogameMetadata):
 	preloaded_microgame = mg
-	sliding_in = false
-	sliding_out = true
-	chilling_a_bit = false;
-	chill_progress = 0;
-	slide_progress = 0
 
 func unload_current_scene():
 	var vpc = $"../MarginContainer/NavdiViewerWindow/ViewportContainer"
@@ -79,48 +71,58 @@ func set_current_microgame(mg: MicrogameMetadata):
 	scene.owner = vp.owner
 	vpc.show()
 	
-	scene.connect("player_won", self, "go_to_next_scene")
-	scene.connect("player_lost", self, "go_to_next_scene")
+	scene.connect("player_won", self, "set_microgame_state", [MgState.DoorsClosing])
+	scene.connect("player_lost", self, "set_microgame_state", [MgState.DoorsClosing])
 	
 	nvw.match_board_size()
 	
 
 func _process(delta):
-	if preloaded_microgame:
-		if sliding_out:
-			slide_progress += delta * 0.85
-			_update_slide_position()
-			if slide_progress >= 1:
-				$"../TrainCar".close_doors();
-				sliding_out = false
-				sliding_in = false
-				chilling_a_bit = true
-		elif sliding_in:
-			slide_progress += delta * 0.5
-			_update_slide_position()
-			if slide_progress >= 1:
-				preloaded_microgame = null
-				sliding_in = false
-				sliding_out = false;
-				chilling_a_bit = false;
-		elif chilling_a_bit:
-			chill_progress += delta
-			if chill_progress >= 2.5:
+	stateTimer += delta # current state: advance timer
+	match microgameState:
+		MgState.ChillingABit:
+			if stateTimer > stateDuration:
+				preload_next_microgame()
 				set_current_microgame(preloaded_microgame)
-				$"../TrainCar".open_doors();
-				chill_progress = 0;
-				chilling_a_bit = false
-				sliding_out = false
-				sliding_in = true
+				preloaded_microgame = null
+				set_microgame_state(MgState.GameSlidingIn)
+		MgState.GameSlidingIn:
+			_update_slide_position()
+			if stateTimer > stateDuration:
+				set_microgame_state(MgState.DoorsOpening)
+		MgState.DoorsOpening:
+			_update_slide_position()
+			if stateTimer > stateDuration:
+				set_microgame_state(MgState.PlayingTheGame)
+		MgState.PlayingTheGame:
+			# do nothing. wait for the game to end itself.
+			pass
+		MgState.DoorsClosing:
+			_update_slide_position()
+			if stateTimer > stateDuration:
+				print("yep ",stateTimer, " > ",stateDuration)
+				set_microgame_state(MgState.GameSlidingOut)
+		MgState.GameSlidingOut:
+			_update_slide_position()
+			if stateTimer > stateDuration:
+				unload_current_scene()
+				set_microgame_state(MgState.ChillingABit)
 
 func _update_slide_position():
 	var nvw = $"../MarginContainer/NavdiViewerWindow"
 	var window_size = $"../MarginContainer".rect_size
 	var offset = Vector2.ZERO
-	if sliding_in:
-		offset = Vector2.RIGHT * window_size * viewport_slide_in.interpolate(slide_progress)
-	elif sliding_out:
-		offset = Vector2.LEFT * window_size * viewport_slide_out.interpolate(slide_progress) * 1.1
+	
+	match microgameState:
+		MgState.GameSlidingIn:
+			offset = Vector2.RIGHT * window_size * viewport_slide_in.interpolate(stateTimer/stateDuration)
+		MgState.PlayingTheGame:
+			offset = Vector2.ZERO # stay in the middle
+		MgState.GameSlidingOut:
+			offset = Vector2.LEFT * window_size * viewport_slide_out.interpolate(stateTimer/stateDuration) * 1.1
+		MgState.ChillingABit:
+			offset = Vector2.LEFT * window_size * 2 # stay off to the left there
+	
 	nvw.refresh()
 	for child in nvw.get_children():
 		child.rect_position += offset
@@ -128,3 +130,23 @@ func _update_slide_position():
 
 func show_error(msg):
 	print("TrainJamMaster load error: %s" % msg)
+
+func set_microgame_state(state):
+	microgameState = state
+	stateTimer = 0.0
+	match microgameState:
+		MgState.GameSlidingIn:
+			stateDuration = 2.0
+		MgState.DoorsOpening:
+			train_car.open_doors();
+			stateDuration = 0.25 # how long is the animation?
+		MgState.PlayingTheGame:
+			stateDuration = -1
+		MgState.DoorsClosing:
+			train_car.close_doors();
+			stateDuration = 1.0 # how long is the animation?
+		MgState.GameSlidingOut:
+			stateDuration = 2.0
+		MgState.ChillingABit:
+			stateDuration = 2.0
+	_update_slide_position()

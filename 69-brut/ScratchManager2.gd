85b1284297aa0dc267bc62213f92f3ad6f22b178
mutmedia <gustavo.ceci95@gmail.com>
2021-07-15 17:25:08 -0400

    score!

--- 69-brut/ScratchManager2.gd
+++ 69-brut/ScratchManager2.gd
@@ -1,8 +1,18 @@
 extends Node2D
 export(Dictionary) var images
-export(Texture) var imageSprite
+export(Texture) var scratchTexture
+export(Texture) var scratchTargets
 export(Texture) var particleTarget
+export(Color) var overflowColor
+export(Color) var missingColor
 export(Color) var scratchColor
+export(NodePath) var differencePath
+export(float) var timeAfterLoss
+
+onready var scracthTargetImages = scratchTargets.get_data()
+onready var differenceNode = get_node(differencePath)
+var differenceImage : Image
+
 var instructions: String
 var rat_attributes = [
 	" cool",
@@ -46,6 +56,9 @@ var was_drawing = false
 var current_line
 var points = 0
 var volume = 0.0
+
+var scracthTarget
+var randomizedImageNumber
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -58,20 +71,75 @@ func _ready():
 	randomize()
 	instructions = "scratch-a-"
 	var names = images.keys()
-	var randomImageNumber = randi()%10 #TODO: make this be an array 
-	#var prize = names[randomImageNumber]
+	randomizedImageNumber = randi()%10 #TODO: make this be an array 
+	#var prize = names[randomizedImageNumber]
 	#instructions += prize
-	$ScratchPrize.frame = randomImageNumber
-#	print(instructions)
+	$ScratchPrize.frame = randomizedImageNumber
+	#	print(instructions)
 	$Instructions.text = instructions
 	previous_pencil_position = get_global_mouse_position()*0.5
 	particlesImage.lock()
+	scracthTargetImages.lock() 
 
+	differenceImage = Image.new()
+	differenceImage.create(160,160,false,Image.FORMAT_RGBA8)
+	differenceImage.fill(Color(0, 0,0 ,0))
+	var t = ImageTexture.new()
+	t.create_from_image(differenceImage)
+	differenceNode.set_texture(t)
+	
 
 func _on_Timer_timeout():
-	print( "EEEEENDjk")
+	waitAndEnd()
+
+func waitAndEnd():
+	compare_scratch()
+	yield(get_tree().create_timer(2.0), "timeout")
 	get_parent().emit_signal("player_won")
 
+func compare_scratch():
+	differenceImage = Image.new()
+	differenceImage.create(160,160,false,Image.FORMAT_RGBA8)
+	# differenceImage.fill(Color(0.2,1,0,0.8))
+
+	differenceImage.lock()
+	var scratchImage = scratchTexture.get_data()
+	scratchImage.lock()
+	var score = 0
+	for i in range(160): 
+		for j in range(160): 
+			var comp_offset_x = i + randomizedImageNumber * 160
+			var color_scratch = scratchImage.get_pixel(i, j)
+			var color_target = scracthTargetImages.get_pixel(comp_offset_x, j)
+
+			# differenceImage.set_pixel(i, j, Color.cyan)
+			# if(color_target.a > 0):
+				# differenceImage.set_pixel(i, j, Color.blue)
+			# if(color_scratch.a > 0):
+				# differenceImage.set_pixel(i, j, Color.yellow)
+			if(color_scratch.a != color_target.a):
+				if(color_scratch.a != 0):
+					differenceImage.set_pixel(i, j, missingColor)
+					score -= 1
+				if(color_scratch.a != 0):
+					differenceImage.set_pixel(i, j, overflowColor)
+					score -= 1
+			else:
+				score += 1
+
+	$Score.text = "YOUR SCORE IS " + str(score)
+
+	scratchImage.unlock()
+	differenceImage.unlock()
+	var t = ImageTexture.new()
+	t.create_from_image(differenceImage)
+	differenceNode.set_texture(t)
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_SPACE:
+			compare_scratch()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -104,8 +172,11 @@ func _process(delta):
 #		print(pencil_delta)
 		$pencil/pencil.rotation_degrees = 30 + pencil_speed.x*0.1
 		particles.direction = -pencil_delta.normalized()
-		var underColor = particlesImage.get_pixelv(particles.global_position)
-		if underColor == scratchColor:
+		if(particles.global_position.x > 0 && particles.global_position.x < 160 && particles.global_position.y > 0 && particles.global_position.y < 160):
+			var underColor = particlesImage.get_pixelv(particles.global_position)
+			if underColor == scratchColor:
+				particles.emitting = false
+		else:
 			particles.emitting = false
 		$pencil.scale.x = pow(1.1, pencil_speed.y*0.01)
 		$pencil.scale.y = 1/$pencil.scale.x
@@ -115,7 +186,6 @@ func _process(delta):
 		
 	previous_pencil_position = $pencil.global_position
 	pass
-
 
 func _draw():
 	pass

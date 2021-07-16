b7f74e96b244903b19f835c1e52aecf2d69da988
pancelor <pancelor@gmail.com>
2021-07-15 19:06:21 -0700

    PCG ice puzzles!
    the algorithm for choosing a "complicated enough" puzzle isn't great; it
    has some bad failure cases and not particularly stellar success cases.
    (it places stuff until there are enough long-enough paths, and then
    places the cheese at one of those places)
    
    But it's a start!

--- 03-pancelor/player.gd
+++ 03-pancelor/player.gd
@@ -7,23 +7,37 @@ export(Array,int)var slide_frames # TODO: update with new art
 var velocity:Vector2=Vector2.ZERO
 var to_next:float=0
 var cell:Vector2
+var start_cell:Vector2
 var visual_offset:Vector2=Vector2.ZERO
 onready var map=$"../State/NavdiBoardTileMap"
 onready var generator=$"../Generator"
+var history=Array()
 
 func _ready():
-	cell=map.world_to_map(position)
+	set_start_cell(map.world_to_map(position))
+
+func set_start_cell(v: Vector2):
+	set_cell(v)
+	start_cell=v
+	history.clear()
+	
+func set_cell(v: Vector2):
+	map._unregister(cell,self)
+	cell=v
 	map._register(cell,self)
 
-func _tow(a,b,rate):
+func snap():
+	history.append(cell)
+
+func approach(a,b,rate):
 	if a+rate<b:return a+rate
 	if a-rate>b:return a-rate
 	return b
 	
 # returns whether the offset is back at zero
 func update_visual_offset():
-	visual_offset.x=_tow(visual_offset.x,0,1)
-	visual_offset.y=_tow(visual_offset.y,0,1)
+	visual_offset.x=approach(visual_offset.x,0,2)
+	visual_offset.y=approach(visual_offset.y,0,2)
 	return visual_offset.x==0 and visual_offset.y==0
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,55 +51,74 @@ func _process(delta):
 		$NavdiSheetSprite.rate = 10
 		return
 		
-	if update_visual_offset():
-		var pin = $NavdiPinQuickPlayer
-		pin.process_pins()
+	var pin = $NavdiPinQuickPlayer
+	pin.process_pins()
+	if pin.a.just_pressed:
+		undo()
+	elif pin.b.just_pressed:
+		restart()
+	elif update_visual_offset():
+		respond_to_movement(pin)
+	position=map.map_to_center(cell)+visual_offset
 
-		var dir=Vector2.ZERO
-		var mytile=map.get_cellv(cell)
-		var nodes=map.get_registered(cell)
-		if $"../cheese" in nodes:
-			$get.play()
-			to_next = 0.1
-#		elif mytile==generator.WIN_TILE:
+func undo():
+	print("undo") # todo play noise
+	if history.size()>0:
+		set_cell(history.pop_back())
+		velocity=Vector2.ZERO
+		visual_offset=Vector2.ZERO
+		
+func restart():
+	print("restart") # todo play noise
+	if velocity.x==0 and velocity.y==0:
+		snap()
+	set_cell(start_cell)
+	velocity=Vector2.ZERO
+	visual_offset=Vector2.ZERO
+	
+func respond_to_movement(pin: NavdiPinQuickPlayer):
+	var dir=Vector2.ZERO
+	var mytile=map.get_cellv(cell)
+#		if mytile==generator.WIN_TILE:
 #			$get.play()
 #			to_next = 0.1
-		elif mytile==generator.ICE_TILE and (velocity.x!=0 or velocity.y!=0):
-			dir=velocity
-			$NavdiSheetSprite.frames = slide_frames
+	if mytile==generator.ICE_TILE and (velocity.x!=0 or velocity.y!=0):
+		dir=velocity
+		$NavdiSheetSprite.frames = slide_frames
+	else:
+		if $"../cheese" in map.get_registered(cell):
+			$get.play()
+			to_next = 0.1
+			return
+		var has_input=true
+		if pin.left.held:
+			dir=Vector2.LEFT
+			$NavdiSheetSprite.flip_h = true
+		elif pin.right.held:
+			dir=Vector2.RIGHT
+			$NavdiSheetSprite.flip_h = false
+		elif pin.up.held:
+			dir=Vector2.UP
+		elif pin.down.held:
+			dir=Vector2.DOWN
 		else:
-			var has_input=true
-			if pin.left.held:
-				dir=Vector2.LEFT
-				$NavdiSheetSprite.flip_h = true
-			elif pin.right.held:
-				dir=Vector2.RIGHT
-				$NavdiSheetSprite.flip_h = false
-			elif pin.up.held:
-				dir=Vector2.UP
-			elif pin.down.held:
-				dir=Vector2.DOWN
-			else:
-				has_input=false
-			if has_input:
-				velocity=dir
-				$NavdiSheetSprite.frames = run_frames
-			else:
-				$NavdiSheetSprite.frames = idl_frames
-		var next_cell=cell+dir
-		var do_move=false
-		if map.get_cellv(next_cell) != generator.WALL_TILE:
-			do_move=true
-		
-		if do_move:
-			visual_offset.x=-map.cell_size.x*dir.x
-			visual_offset.y=-map.cell_size.y*dir.y
-			map._unregister(cell,self)
-			cell=next_cell
-			map._register(cell,self)
+			has_input=false
+		if has_input:
+			snap()
+			velocity=dir
+			$NavdiSheetSprite.frames = run_frames
 		else:
-			visual_offset.x=0.25*map.cell_size.x*dir.x
-			visual_offset.y=0.25*map.cell_size.y*dir.y
-			velocity=Vector2.ZERO
-	position=map.map_to_center(cell)+visual_offset
+			$NavdiSheetSprite.frames = idl_frames
+	var next_cell=cell+dir
+	var do_move=false
+	if map.get_cellv(next_cell) != generator.WALL_TILE:
+		do_move=true
 	
+	if do_move:
+		visual_offset.x=-map.cell_size.x*dir.x
+		visual_offset.y=-map.cell_size.y*dir.y
+		set_cell(next_cell)
+	else:
+		visual_offset.x=0.25*map.cell_size.x*dir.x
+		visual_offset.y=0.25*map.cell_size.y*dir.y
+		velocity=Vector2.ZERO
